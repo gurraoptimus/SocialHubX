d03e606a248fa0af4ce4816692a9456a52d5594c
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2025-03-28 14:20:16 +0100

    SocialHubX

--- Linktree/PyTree.py
+++ Linktree/PyTree.py
@@ -9,7 +9,7 @@ from io import BytesIO
 # ============================
 #linktree_username = "linktree"
 linktree_username = input("Enter your Linktree username: ")  # User can input their Linktree username
-linktree_url = f"https://www.linktree.com/@{linktree_username}/"
+linktree_url = f"https://www.linktree.com/{linktree_username}/"
 
 # ============================
 # 📸 Step 2: Create QR Code
